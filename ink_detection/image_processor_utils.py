f71f8f15d7470ae4f0bcca29908932e7f06ef44c
Wishmitha Mendis <wishmitha.14@cse.mrt.ac.lk>
2023-07-15 15:36:18 +0100

    Cropped resize images for windows of 70x70 for stride of 10

--- ink_detection/image_processor_utils.py
+++ ink_detection/image_processor_utils.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
+import os
+import shutil
 
 from PIL import Image
-import os
 
 
 def resize_image(image_path, factor):
@@ -42,8 +43,66 @@ def resize_image_set(input_path, output_path):
                 reszied_image.save(output_file)
 
 
-# Example usage
+def create_image_crops(image, window_size, stride):
+    crops = []
+    height, width = image.shape[:2]
+
+    # Calculate the number of crops in each dimension
+    num_crops_height = (height - window_size) // stride + 1
+    num_crops_width = (width - window_size) // stride + 1
+
+    for i in range(num_crops_height):
+        for j in range(num_crops_width):
+            # Calculate the starting indices of the crop
+            start_h = i * stride
+            start_w = j * stride
+
+            # Extract the crop from the image
+            crop = image[start_h:start_h + window_size, start_w:start_w + window_size]
+            crops.append(crop)
+
+    return np.array(crops)
+
+
+def crop_image_set(input_path, output_path, window_size, stride):
+    # Iterate through folders and subfolders
+    for root, dirs, files in os.walk(input_path):
+        # Create corresponding folder structure in the output path
+        relative_path = os.path.relpath(root, input_path)
+        output_folder = os.path.join(output_path, relative_path)
+        os.makedirs(output_folder, exist_ok=True)
+
+        # Process image files in the current folder
+        for file in files:
+            # Check if the file is a PNG or TIFF image
+            if file.endswith(".png") or file.endswith(".tif"):
+                # Read the image
+                image_path = os.path.join(root, file)
+                image = cv2.imread(image_path)
+
+                # Generate crops
+                crops = create_image_crops(image, window_size, stride)
+
+                # Create output folder for the current image
+                image_output_folder = os.path.join(output_folder, os.path.splitext(file)[0] + "_crops")
+                os.makedirs(image_output_folder, exist_ok=True)
+
+                # Save the crops
+                for i, crop in enumerate(crops):
+                    crop_path = os.path.join(image_output_folder, f"crop_{i}.png")
+                    cv2.imwrite(crop_path, crop)
+
+                print(f"Processed {file}: {len(crops)} crops saved in {image_output_folder}")
+
+
 if __name__ == '__main__':
-    input_path = "../../Datasets/vesuvius-challenge-ink-detection/test"
-    output_path = "../../Datasets/vesuvius-challenge-ink-detection/resized_test"
-    resize_image_set(input_path, output_path)
+
+    # input_path = "../../Datasets/vesuvius-challenge-ink-detection/train"
+    # output_path = "../../Datasets/vesuvius-challenge-ink-detection/resized_train"
+    # resize_image_set(input_path, output_path)
+
+    # input_path = "../../Datasets/vesuvius-challenge-ink-detection/resized_train"
+    # output_path = "../../Datasets/vesuvius-challenge-ink-detection/cropped_train_x"
+    # crop_image_set(input_path, output_path, window_size=70, stride=10)
+
+    pass
