21a947c775dbf60f245b2fe8b048f8bfd5e2ab6c
Wishmitha Mendis <wishmitha.14@cse.mrt.ac.lk>
2023-07-16 14:57:29 +0100

    Stitch results for two test fragments

--- ink_detection/ink_detection_utils.py
+++ ink_detection/ink_detection_utils.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import cv2
+import re
 
 
 def load_dataset(dataset_path, dataset):
@@ -31,7 +32,7 @@ def load_dataset(dataset_path, dataset):
 
         # Load the target label image and train label images
         for file_name in file_names:
-            print('Fragment:',frag,'Loaded:',file_name)
+            print('Fragment:', frag, 'Loaded:', file_name)
             # Construct the file paths for the target label and train labels
             target_label_path = os.path.join(inklabels_folder, file_name)
             train_label_paths = [os.path.join(surface_volume_folder, f'{i:02d}_crops', file_name) for i in range(65)]
@@ -51,3 +52,54 @@ def load_dataset(dataset_path, dataset):
         dataset.append([train_labels, target_labels])
 
     return dataset
+
+
+def get_crop_position(image_height, image_width, crop_number, window_size=70, stride=10):
+    crop_row = crop_number // ((image_height - window_size) // stride + 1)
+    crop_col = crop_number % ((image_width - window_size) // stride + 1)
+    x = crop_col * stride
+    y = crop_row * stride
+    return x, y
+
+
+def stitch_crops(image_height, image_width, image_crops, crop_positions):
+    # Initialize output image with the desired size
+    output_size = (image_height, image_width)
+    output_image = np.zeros(output_size)
+
+    # Iterate over the image crops and their positions
+    for crop, position in zip(image_crops, crop_positions):
+        crop = crop.cpu().numpy()[0][0]
+        crop_height, crop_width = crop.shape[:2]
+        x, y = get_crop_position(image_height, image_width, position)
+
+        # Calculate the region occupied by the crop in the output image
+        y_start, y_end = y, y + crop_height
+        x_start, x_end = x, x + crop_width
+
+        # Add the pixels of the crop to the corresponding region in the output image
+        if crop.shape == output_image[y_start:y_end, x_start:x_end].shape:
+            output_image[y_start:y_end, x_start:x_end] = output_image[y_start:y_end, x_start:x_end] + crop
+
+    # Clip the pixel values to ensure they are within the desired range
+    output_image = np.clip(output_image, 0, 255)
+
+    # Convert the output image to the appropriate data type if needed
+    output_image = output_image.astype(np.uint8)
+
+    return  output_image
+
+
+def get_crop_numer_list(file_name):
+    # Read the text file
+    with open(file_name, 'r') as file:
+        content = file.read()
+
+    # Extract the middle numbers using regular expression
+    pattern = r'crop_(\d+)\.png'
+    matches = re.findall(pattern, content)
+
+    # Convert the matched strings to integers
+    numbers = [int(match) for match in matches]
+
+    return numbers
