f0898357a35cb98b17ffd53edc4563ad87b88079
Wishmitha Mendis <wishmitha.14@cse.mrt.ac.lk>
2023-07-17 17:05:05 +0100

    Generate results for the validation set with simple UNet model

--- ink_detection/ink_detection_utils.py
+++ ink_detection/ink_detection_utils.py
@@ -27,11 +27,19 @@ def load_dataset(dataset_path, dataset):
         with open(masked_crops_path, 'r') as file:
             file_names = [line.strip() for line in file]
 
+        file_names = sorted(file_names, key=lambda x: int(re.search(r"\d+", x).group()))
+
         target_labels = []
         train_labels = []
+        crop_nums = []
 
         # Load the target label image and train label images
         for file_name in file_names:
+
+            match = re.search(r"\d+", file_name)
+            if match:
+                crop_no = int(match.group())
+
             print('Fragment:', frag, 'Loaded:', file_name)
             # Construct the file paths for the target label and train labels
             target_label_path = os.path.join(inklabels_folder, file_name)
@@ -45,13 +53,73 @@ def load_dataset(dataset_path, dataset):
             train_label_images = [cv2.imread(train_label_path) for train_label_path in train_label_paths]
             train_labels.append(train_label_images)
 
+            crop_nums.append(crop_no)
+
         # Convert the lists to numpy arrays for convenience
         target_labels = np.array(target_labels)
         train_labels = np.array(train_labels)
+        crop_nums = np.array(crop_nums)
 
-        dataset.append([train_labels, target_labels])
+        dataset.append([train_labels, target_labels, crop_nums])
 
     return dataset
+
+
+# def load_dataset_with_validation_set(dataset_path, dataset, validation_set):
+#     if dataset == 'train':
+#         path = os.path.join(dataset_path, 'cropped_train')
+#         frags = ['1', '2', '3']
+#     else:
+#         path = os.path.join(dataset_path, 'cropped_test')
+#         frags = ['a', 'b']
+#
+#     # Read the file names from masked_crops.txt
+#
+#     dataset = []
+#
+#     for frag in frags:
+#
+#         frag_path = os.path.join(path, frag)
+#
+#         masked_crops_path = os.path.join(frag_path, 'masked_crops.txt')
+#         inklabels_folder = os.path.join(frag_path, 'inklabels_crops')
+#         surface_volume_folder = os.path.join(frag_path, 'surface_volume')
+#
+#         with open(masked_crops_path, 'r') as file:
+#             file_names = [line.strip() for line in file]
+#
+#         target_labels = []
+#         train_labels = []
+#
+#         # Load the target label image and train label images
+#         for file_name in file_names:
+#
+#             match = re.search(r"\d+", file_name)
+#             if match:
+#                 crop_no = int(match.group())
+#
+#             if crop_no not in validation_set:
+#
+#                 print('Fragment:', frag, 'Loaded:', file_name)
+#                 # Construct the file paths for the target label and train labels
+#                 target_label_path = os.path.join(inklabels_folder, file_name)
+#                 train_label_paths = [os.path.join(surface_volume_folder, f'{i:02d}_crops', file_name) for i in range(65)]
+#
+#                 # Load the target label image
+#                 target_label_image = cv2.imread(target_label_path)
+#                 target_labels.append(target_label_image)
+#
+#                 # Load the train label images
+#                 train_label_images = [cv2.imread(train_label_path) for train_label_path in train_label_paths]
+#                 train_labels.append(train_label_images)
+#
+#         # Convert the lists to numpy arrays for convenience
+#         target_labels = np.array(target_labels)
+#         train_labels = np.array(train_labels)
+#
+#         dataset.append([train_labels, target_labels])
+#
+#     return dataset
 
 
 def get_crop_position(image_height, image_width, crop_number, window_size=70, stride=10):
@@ -87,7 +155,7 @@ def stitch_crops(image_height, image_width, image_crops, crop_positions):
     # Convert the output image to the appropriate data type if needed
     output_image = output_image.astype(np.uint8)
 
-    return  output_image
+    return output_image
 
 
 def get_crop_numer_list(file_name):
